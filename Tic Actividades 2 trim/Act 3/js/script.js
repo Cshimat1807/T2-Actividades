25e1cdc4ac1008e472431a55a3b48f52e8e7299e
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-10 19:53:22 +0100

    Add files via upload

---
+++ Tic Actividades 2 trim/Act 3/js/script.js
@@ -0,0 +1,8 @@
+function pedirDatos() {
+let nombre;
+nombre = prompt("¿Cómo te llamas?");
+alert ("Hola " + nombre );
+let edad;
+edad = prompt("¿Cuanto años tienes?")
+alert ("veo que tienes " +  edad )
+}
