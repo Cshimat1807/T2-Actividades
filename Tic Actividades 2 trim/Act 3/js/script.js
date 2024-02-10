af5ce0ef970160a7eeb3dbc81b7186f17bdea1f7
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-10 19:54:50 +0100

    Delete Tic Actividades 2 trim directory

--- Tic Actividades 2 trim/Act 3/js/script.js
+++
@@ -1,8 +0,0 @@
-function pedirDatos() {
-let nombre;
-nombre = prompt("¿Cómo te llamas?");
-alert ("Hola " + nombre );
-let edad;
-edad = prompt("¿Cuanto años tienes?")
-alert ("veo que tienes " +  edad )
-}
--- Tic Actividades 2 trim/Act 4/js/script.js
+++
@@ -1,8 +0,0 @@
-function calcular() {
-let a;
-let b;
-
-a = parseInt(prompt("Introduce el primer número:"), 10 );
-b = parseInt(prompt("Introduce el segundo número:"), 10 );
-alert ("El primer número es: " + a + "\nEl segundo número es: " + b + "\nLa suma es: " + (a+b) + "\nLa resta es: " + (a-b) + "\nElproducto es: " + (a*b) + "\nLa división es: " + (a/b) +"\nEl resto es: " + (a%b));
-}
