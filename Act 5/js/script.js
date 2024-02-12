a0d2fe93cb8d627b4352f2b8c51ae8971a0ac90c
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-12 12:56:12 +0100

    Update script.js

--- Act 5/js/script.js
+++ Act 5/js/script.js
@@ -3,5 +3,5 @@ let n1;
 let n2;
 n1 = document.getElementById("n1").value;
 n2 = parseInt(document.getElementById ("n2").value, 0);
-document.getElementById("mensaje").innerHTML = "El primer número es: " + n1 + "\nEl segundo número es: " + n2 + "\nLa suma es: " + (n1+n2) + "\nLa resta es: " + (n1-n2) + "\nElproducto es: " + (n1*n2) + "\nLa división es: " + (n1/n2) +"\nEl resto es: " + (n1%n2)
+document.getElementById("mensaje").innerHTML = "El primer número es: " + n1 + "<br>El segundo número es: " + n2 + "<br>La suma es: " + (n1+n2) + "<br>La resta es: " + (n1-n2) + "<br>Elproducto es: " + (n1*n2) + "<br>La división es: " + (n1/n2) +"<br>El resto es: " + (n1%n2)
 }
