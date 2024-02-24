ccd7643a352856e5bc6b5bad9382928b7e181e3d
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-24 15:53:06 +0100

    Update script.js

--- Act 8/js/script.js
+++ Act 8/js/script.js
@@ -14,41 +14,40 @@ function MensajeIf() {
         document.getElementById("solucion").innerHTML = "El valor introducido no es válido";
     }
 }
-
   function MensajeSwitch() {
     let hora = prompt("¿Qué hora es?");
     switch (hora) {
-     case "7":
-     case "8":
-     case "9":
-     case "10":
-     case "11":
+         case "7":
+         case "8":
+         case "9":
+         case "10":
+         case "11":
         document.getElementById("solucion").innerHTML = "Buenos días";
      break;
-     case "12":
-     case "13":
-     case "14":
-     case "15":
-     case "16":
-     case "17":
-     case "18":
-     case "19":
-     case "20":
+         case "12":
+         case "13":
+         case "14":
+         case "15":
+         case "16":
+         case "17":
+         case "18":
+         case "19":
+         case "20":
         document.getElementById("solucion").innerHTML = "Buenas tardes";
      break;
-     case "21":
-     case "22":
-     case "23":
-     case "24":
-     case "0":
-     case "1":
-     case "2":
-     case "3":
-     case "4":
-     case "5":
-     case "6":
+         case "21":
+         case "22":
+         case "23":
+         case "24":
+         case "0":
+         case "1":
+         case "2":
+         case "3":
+         case "4":
+         case "5":
+         case "6":
         document.getElementById("solucion").innerHTML = "Buenas noches";
-      break;
+    break;
       default:
           document.getElementById("solucion").innerHTML = "El valor introducido no es válido";
     }
