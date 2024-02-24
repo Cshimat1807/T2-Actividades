50035d2ef49c7f79b66d72ed7fc4fbc626dc0c6d
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-24 16:00:14 +0100

    Update script.js

--- Act 10/js/script.js
+++ Act 10/js/script.js
@@ -1,11 +1,11 @@
 function Calificar() {
     let alumnos = ["Julia", "Alba G", "Rosalin", "Alvaro"];
     let nota = [];
-        nota[1]=parseInt(prompt("¿Cuál es la nota de " + alumnos[0] + "?"));
+        nota[1]=parseInt(prompt("¿Cuál es la nota de " + alumnos[1] + "?"));
 
-        nota[2]=parseInt(prompt("¿Cuál es la nota de " + alumnos[1] + "?"));
+        nota[2]=parseInt(prompt("¿Cuál es la nota de " + alumnos[2] + "?"));
 
-        nota[3]=parseInt(prompt("¿Cuál es la nota de " + alumnos[2] + "?"));
+        nota[3]=parseInt(prompt("¿Cuál es la nota de " + alumnos[3] + "?"));
 
-        nota[4]=parseInt(prompt("¿Cuál es la nota de " + alumnos[3] + "?"));
+        nota[4]=parseInt(prompt("¿Cuál es la nota de " + alumnos[4] + "?"));
 }
