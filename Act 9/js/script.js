93727877848f30d3766ec9c0562ba3d368e71b9e
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-24 15:57:58 +0100

    Update script.js

--- Act 9/js/script.js
+++ Act 9/js/script.js
@@ -1,11 +1,12 @@
 function calcular() {
-    let numero = parseInt(document.getElementById("numero").value, 10);
-	resultado = 1;
-	iteracion = 1;
-
-	while (iteracion <= numero) {
-		iteracion = iteracion + 1;
-		resultado = resultado * iteracion;
-	}
+     let resultado, iteracion, numero;
+    	resultado = 1;
+   	 iteracion = 1;
+   	 numero = parseInt(document.getElementById("numero").value, 10);
+    
+	while ( iteracion < numero ) {
+        	iteracion = iteracion + 1;
+        	resultado = resultado * iteracion;
+    }
     document.getElementById("solucion").innerHTML = "El factorial del "+ numero + " es "+ resultado;
 }
