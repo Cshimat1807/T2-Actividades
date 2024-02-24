227db74456d8ec4d766e9538f8bf24ae81f98e63
Cshimat1807 <151845097+Cshimat1807@users.noreply.github.com>
2024-02-24 15:23:17 +0100

    Add files via upload

---
+++ Act 10/js/script.js
@@ -0,0 +1,11 @@
+function Calificar() {
+    let alumnos = ["Julia", "Alba G", "Rosalin", "Alvaro"];
+    let nota = [];
+        nota[1]=parseInt(prompt("¿Cuál es la nota de " + alumnos[0] + "?"));
+
+        nota[2]=parseInt(prompt("¿Cuál es la nota de " + alumnos[1] + "?"));
+
+        nota[3]=parseInt(prompt("¿Cuál es la nota de " + alumnos[2] + "?"));
+
+        nota[4]=parseInt(prompt("¿Cuál es la nota de " + alumnos[3] + "?"));
+}
---
+++ Act 9/js/script.js
@@ -0,0 +1,10 @@
+function calcular() {
+    
+    r = 1;
+    iteracion = 1;
+    numero = parseInt(document.getElementById("solucion").value, 10);
+ 
+    
+    
+    document.getElementById("solucion").innerHTML = "El factorial del "+ numero + " es "+ resultado;
+}
